a73dbbae3b0487214a0d3b8dcfb3417e94a98990
Guilherme Scatuzzi Alves Ramos <57189743+scazr@users.noreply.github.com>
2026-05-01 20:09:11 -0300

    Remove input error check for n > 500
    Removed input error check for values greater than 500.

--- 2023_fase1/solutions/foraminis.java
+++ 2023_fase1/solutions/foraminis.java
@@ -10,10 +10,6 @@ public class foraminis {
   public static void main(String[] args) throws IOException {
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(in.readLine());
-    if (n > 500) {
-      System.out.println("Input error");
-      return;
-    }
 
     for (int i = 0; i < n; i++) {
       double[] s = new double[8];
